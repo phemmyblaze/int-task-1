daa8556c39d319f3e885831f7392d85db6691fa7
Akinuliola Akinbobola <akinuliola_a@yahoo.com>
2022-04-13 11:46:52 +0100

    update made

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import React, { useState } from "react";
+import { Card, Container } from "react-bootstrap";
+import Gifts from "./Gifts";
 import "./App.css";
-import Gift from "./Gift";
 
-const App = () => {
-  return <Gift />;
-};
+function App() {
+  const [gifts] = useState(Gifts);
+  return (
+    <Container className="p-5">
+      <h2>Currencies</h2>
+      <div className="count">
+        {gifts.map((item) => (
+          <Card style={{ width: "18rem" }} key={item.id}>
+            <Card.Body>
+              <Card.Title>{item.title}</Card.Title>
+              <div className="count">
+                {item.currencies.map((arrayItem, index) => (
+                  <p className="m-3 p-2 " key={index}>
+                    {arrayItem}
+                  </p>
+                ))}
+              </div>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
+    </Container>
+  );
+}
 
 export default App;
